9c945d258650feafbe0f94de0b2c45401d3e51ab
dcy520hmn <1103334087@qq.com>
2020-09-24 13:49:11 +0800

    vue路由中的children

---
+++ leading_End/vue_len/user/LayOut.js
@@ -0,0 +1,10 @@
+const layOut = {
+    template:'\
+        <div>\
+            <h1>layOut1</h1>\
+            <router-link to="/layout/login">login</router-link>\
+            <router-link to="/layout/register">sign</router-link>\
+            <router-view></router-view>\
+        </div>\
+        '
+}
